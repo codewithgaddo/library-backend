9b7796ac407373927b364ec0bf60732e492b5cd4
codewithgaddo <vxforothers@gmail.com>
2025-06-23 19:08:43 +0200

    Bug fixes v2

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -14,8 +14,8 @@ router.use(authMiddleware);
 
 router.post("/",roleAuth(["librarian", "admin"]), addCategory);
 router.get("/", getCategories);
-router.put("/:id", roleAuthMiddleware(["admin", "librarian"]), updateCategory);
-router.delete("/:id", roleAuthMiddleware(["admin", "librarian"]), deleteCategory);
+router.put("/:id", roleAuth(["admin", "librarian"]), updateCategory);
+router.delete("/:id", roleAuth(["admin", "librarian"]), deleteCategory);
 
 
 module.exports = router;
